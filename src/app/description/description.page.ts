0b9c1acc5e29e4f533500dc4ccbb75711480f376
Alvaro Cantarino <alvaro.cj05@gmail.com>
2020-11-05 07:59:43 +0100

    Integrating camera plugin

--- src/app/description/description.page.ts
+++ src/app/description/description.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {Pokemon} from '../interfaces/pokemon';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 @Component({
   selector: 'app-description',
   templateUrl: './description.page.html',
@@ -10,11 +12,28 @@ import {Pokemon} from '../interfaces/pokemon';
 export class DescriptionPage implements OnInit {
 
   pokemonDes: Pokemon;
+  public image: string;
 
-  constructor(private route: Router) {
+  constructor(private route: Router, private cam: Camera) {
     this.pokemonDes = (this.route.getCurrentNavigation().extras.state as {info: Pokemon}).info;
+    this.getPicture();
   }
 
   ngOnInit() {}
+  getPicture(){
+    const cameraOptions = {
+      sourceType: this.cam.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.cam.DestinationType.FILE_URI,
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: this.cam.EncodingType.JPEG,
+      correctOrientation: true
+    };
+
+    this.cam.getPicture(cameraOptions)
+        .then(fileUri => this.image = fileUri,
+            err => console.log(err));
+  }
 
 }
