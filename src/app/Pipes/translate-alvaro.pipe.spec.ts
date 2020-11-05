b04aede9834818e4c8668f70a9a31538d05823cb
Alvaro Cantarino <alvaro.cj05@gmail.com>
2020-11-05 16:28:51 +0100

    Fixing UI

---
+++ src/app/Pipes/translate-alvaro.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TranslateAlvaroPipe } from './translate-alvaro.pipe';
+
+describe('TranslateAlvaroPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TranslateAlvaroPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
