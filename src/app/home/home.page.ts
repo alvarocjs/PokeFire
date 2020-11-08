40a1db4ded4466f8de0a0b99fde5577a6347cc0e
Alvaro Cantarino <alvaro.cj05@gmail.com>
2020-11-08 20:04:50 +0100

    Fixing AndroidX problems

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,6 @@ import {PokeApiService} from '../services/poke-api.service';
 import {PokeList} from '../interfaces/poke-list';
 import {Pokemon} from '../interfaces/pokemon';
 import {Router} from '@angular/router';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
